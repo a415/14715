90eb0b24b75833a410ff29947a7b6fe468307f8d
kli <kli14712@163.com>
2019-09-08 20:51:22 +0800

    ok

---
+++ setting.py
@@ -0,0 +1,14 @@
+#httpserver配置
+
+HOST = "0.0.0.0"
+PORT = 8000
+ADDR = (HOST,PORT)
+
+
+#WebFrame设置
+frame_ip = "127.0.0.1"
+frame_port = 8080
+frame_addr = (frame_ip,frame_port)
+
+
+STATIC_DIR = './static'
